00b488426a7549505c8eb0cc1c1955a6e842cf01
CruzJeff <26846343+CruzJeff@users.noreply.github.com>
2017-09-28 02:02:15 -0400

    Update part1.py

--- hw1/part1.py
+++ hw1/part1.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Sep 19 19:28:29 2017
 
-@author: Jeffrey Cruz
+@author: User
 """
 
 
@@ -16,8 +16,16 @@ built in method as before, and compare it to the built in model. Ballpark, you s
 accuracy. You need not exactly replicate the results of the built-in classifier - but yours should
 return correct predictions according to your distance metric.
 '''
+#import libraries
 import numpy as np
 
+#euclid distance function necessary for KNN algorithm
+def Euclid_distance(a,b):  #Calculate Distance using Numpy library
+    a = np.array(a)
+    b = np.array(b)
+    return np.linalg.norm(a - b)
+
+#KNN Classifier
 class MyNearestNeighborClassifier():
     
     def __init__(self, n_neighbors=1):  #Constructor
@@ -28,12 +36,6 @@ class MyNearestNeighborClassifier():
         self.y = y_train
         if self.X is not list: #Convert Numpy Array to List if needed
             self.X = self.X.tolist()
- 
-    def Euclid_distance(a,b):  #Calculate Distance using Numpy library
-        a = np.array(a)
-        b = np.array(b)
-        return np.linalg.norm(a - b)
-    
          
     def predict(self, point):  #Make Predictions
             
@@ -56,21 +58,25 @@ class MyNearestNeighborClassifier():
         
         return sorted_nearest[0][0] #Predict the majority class of the nearest neighbors
     
-    
+#importing the dataset
 from sklearn import datasets
 iris = datasets.load_iris()
 iris_X = iris.data
 iris_y = iris.target
+#generating random permutations
 np.random.seed(0)
 indices = np.random.permutation(len(iris_X))
+
+#splitting into training and testing data sets
 iris_X_train = iris_X[indices[:-10]]
 iris_y_train = iris_y[indices[:-10]]
 iris_X_test  = iris_X[indices[-10:]]
 iris_y_test  = iris_y[indices[-10:]]
 
+#fitting the classifier on training set
 clf = MyNearestNeighborClassifier(3)
 clf.fit(iris_X_train, iris_y_train)
-
+#making predictions on test set
 predictions = []
 for point in iris_X_test:
     predictions.append(clf.predict(point))
@@ -80,6 +86,7 @@ print("Iris_Y_Test set is actually", iris_y_test)
 
 #iris_y_test is array([1, 1, 1, 0, 0, 0, 2, 1, 2, 0])
 
+#Getting predictions from official KNN Classifier
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier()
 knn.fit(iris_X_train, iris_y_train) 
